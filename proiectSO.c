a24b4a99bdc4b04f3c77f0167c2c33e820225ba7
vladdragomir1 <163445817+vladdragomir1@users.noreply.github.com>
2024-04-19 09:10:37 +0300

    Update proiectSO.c

--- proiectSO.c
+++ proiectSO.c
@@ -1,15 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <sys/types.h>
 #include <time.h>
 #include <fcntl.h>
-#include <pwd.h>
-#include <grp.h>
 
 void createSnapshot(const char *directoryPath) {
     DIR *dir;
@@ -19,69 +15,83 @@ void createSnapshot(const char *directoryPath) {
     time_t t;
     struct tm *tmp;
     char lastModifiedTime[50];
-
+    // Open the specified directory
     dir = opendir(directoryPath);
     if(dir == NULL) {
         perror("Unable to open directory");
         exit(EXIT_FAILURE);
     }
 
+    // Open the Snapshot.txt file
     snprintf(filePath, sizeof(filePath), "%s/Snapshot.txt", directoryPath);
-    int snapshotFile = open(filePath, O_WRONLY, S_IWUSR); 
+    FILE *snapshotFile = fopen(filePath, "w"); // Change open to fopen and use "w" to write to the file.
 
-    if(snapshotFile < 0) {
+    if(snapshotFile == NULL) {
         perror("Unable to create snapshot file");
         exit(EXIT_FAILURE);
     }
-
     // Iterate over each entry in the directory
     while((entry = readdir(dir)) != NULL) {
         snprintf(filePath, sizeof(filePath), "%s/%s", directoryPath, entry->d_name);
-
+        // Get file stats
         if(stat(filePath, &fileInfo) == 0) {
             t = fileInfo.st_mtime;
             tmp = localtime(&t);
             strftime(lastModifiedTime, sizeof(lastModifiedTime), "%Y-%m-%d %H:%M:%S", tmp);
             // Write metadata to Snapshot.txt
             if(S_ISDIR(fileInfo.st_mode)) {
-                fprintf(snapshotFile, "Directory: %s, Last Modified: %s\n", entry->d_name, lastModifiedTime);
+                write(snapshotFile, "Directory: %s, Last Modified: %s\n", entry->d_name, lastModifiedTime);
             } else {
                 fprintf(snapshotFile, "File: %s, Size: %ld, Last Modified: %s\n", entry->d_name, fileInfo.st_size, lastModifiedTime);
             }
         }
     }
+    //Cleanup
+    fclose(snapshotFile);
     closedir(dir);
 }
+
+/*
+void compare_snapshot {
+}
+*/
 
 int main(int argc, char *argv[]) {
 
     if(argc > 10) {
+        fprintf(stderr, "Usage: %s \n", argv[0]);
         fprintf(stderr, "Usage: %s [directory_path]...\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    int pid;
+    pid_t pid;
     for(int i = 1; i < argc; i++) {
+        createSnapshot(argv[i]);
         pid = fork();
-        
         if(pid == -1) {
             perror("fork failed");
             exit(EXIT_FAILURE);
         } else if(pid == 0) {
             // This is the child process
             createSnapshot(argv[i]);
-            printf("Snapshot for %s created succesfully.\n", argv[i]);
             _exit(EXIT_SUCCESS); // Use _exit in child to prevent flushing of stdio buffers from parent
         }
+        printf("Snapshot for %s created succesfully.\n", argv[i]);
+        // Parent will continue to the next iteration without waiting here
     }
-    
+
+
     // Parent waits for all child processes to finish
     int status;
+    int ct = 0;
     while ((pid = wait(&status)) > 0) {
+        ct++;
         if(WIFEXITED(status)) {
-            printf("Child terminated with PID %ld and exit code %d.\n", (long)pid, WEXITSTATUS(status));
+            printf("Child proccess %d terminated with PID %ld and exit code %d.\n", ct, (long)pid, WEXITSTATUS(status));
         }
     }
+
+    createSnapshot(argv[1]);
 
     return EXIT_SUCCESS;
 }
