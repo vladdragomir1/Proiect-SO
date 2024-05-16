a4dd38d8b1f686da968019928fa0b773166e7b28
vladdragomir1 <163445817+vladdragomir1@users.noreply.github.com>
2024-05-16 17:04:09 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,21 @@
+//SCRIPT.SH
+
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <file_path>"
+    exit 1
+fi
+
+KEYWORDS=("corrupt" "dangerous" "risk" "attack" "malware" "malicious")
+FILE=$1
+
+for kw in "${KEYWORDS[@]}"; do
+    if grep -qi $kw "$FILE"; then
+        echo "malicious"
+        exit 0
+    fi
+done
+
+echo "clean"
+exit 0
